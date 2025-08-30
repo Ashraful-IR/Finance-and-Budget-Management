0acfacfa0ff2471c0c104ec85dc408244eaef1d2
Ashraful-IR <rifo0118@gmail.com>
2025-08-30 16:16:22 +0600

    a

---
+++ Manager Dash/dash.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | Manager</title>
+    <link rel="stylesheet" href="../Manager Dash/dashstyle.css" type="text/css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="menu">
+            
+        </div>
+
+        </div>
+    </div>
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>

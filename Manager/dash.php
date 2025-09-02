fae4c0b6d7d8ce8d32aaa74f07b8a0864dade52a
Ashraful-IR <rifo0118@gmail.com>
2025-09-03 02:44:31 +0600

    maager dash with menue button

--- Manager/dash.php
+++ Manager/dash.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Manager</title>
-    <link rel="stylesheet" href="../Manager Dash/dashstyle.css" type="text/css">
+    <link rel="stylesheet" href="../Manager/dashstyle.css" type="text/css">
 </head>
 
 <body>

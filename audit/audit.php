62cb7cb45f7edda562b8a6d3b6b92c1b3be50174
ariffaysal <ariffaysal001@gmai.com>
2025-09-06 11:33:42 +0600

    cmt

--- audit/audit.php
+++ audit/audit.php
@@ -40,6 +40,64 @@
                 <ion-icon name="person-outline"></ion-icon> Account
             </a>
         </div>
+        <table class="greenTable">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>F_Name</th>
+      <th>L_NAME</th>
+      <th>EMAIL</th>
+      <th>PHONE</th>
+      <th>DEPT</th>
+      <th>STATUS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>cell1_1</td>
+      <td>cell2_1</td>
+      <td>cell3_1</td>
+      <td>cell4_1</td>
+      <td>cell5_1</td>
+      <td>cell6_1</td>
+      <td>cell7_1</td>
+    </tr>
+    <tr>
+      <td>cell1_2</td>
+      <td>cell2_2</td>
+      <td>cell3_2</td>
+      <td>cell4_2</td>
+      <td>cell5_2</td>
+      <td>cell6_2</td>
+      <td>cell7_2</td>
+    </tr>
+    <tr>
+      <td>cell1_3</td>
+      <td>cell2_3</td>
+      <td>cell3_3</td>
+      <td>cell4_3</td>
+      <td>cell5_3</td>
+      <td>cell6_3</td>
+      <td>cell7_3</td>
+    </tr>
+    <tr>
+      <td>cell1_4</td>
+      <td>cell2_4</td>
+      <td>cell3_4</td>
+      <td>cell4_4</td>
+      <td>cell5_4</td>
+      <td>cell6_4</td>
+      <td>cell7_4</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td colspan="7">
+        <div class="links"><a href="#">&laquo;</a> <a class="active" href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">&raquo;</a></div>
+      </td>
+    </tr>
+  </tfoot>
+</table>
         
 
     </div>

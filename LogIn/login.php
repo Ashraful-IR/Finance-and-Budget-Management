a5f1283d1f9ec946a9e35dd92550adc9098742c8
shahriar-rafi33 <shahriar.rafi1406315@gmail.com>
2025-09-07 19:11:41 +0600

    Employee Login fixed

--- LogIn/login.php
+++ LogIn/login.php
@@ -33,8 +33,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 } elseif ($role === "manager") {
                     header("Location: ../Manager/dash.php");
                     exit();
-                }elseif ($role === "Employee") {
-                    header("Location: ../Employee/employee-dashboard.php");
+                }elseif ($role === "employee") {
+                    header("Location: ../Employee/Employee-dashboard.php");
                     exit();
                 }
                 
